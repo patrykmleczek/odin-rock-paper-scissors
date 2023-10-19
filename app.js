968e0e3c82c558c7e44ad125710e5ef3b5d67cfc
patrykmleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-10-19 23:54:15 +0200

    feat: add one round of game

--- app.js
+++ app.js
@@ -5,14 +5,44 @@ function getComputerChoice() {
     let randomMove = Math.floor(Math.random() * moves.length);
     console.log(moves[randomMove]);
     return moves[randomMove];
-
 }
 // ask player for input one of moves: rock/paper/scissors ->  prompt
-let playerSelection = prompt("Is it rock, paper or scissors?", " ")
+let playerSelection = prompt("Is it rock, paper or scissors?", "paper")
 //answers from should always be lowercase
 playerSelection = playerSelection.toLowerCase()
-// console.log(playerSelection)
-// play round and compare moves, check who wins
+console.log(playerSelection)
+// play round and compare moves, check who wins 
+function playRound(computerSelection, playerSelection) {
+    let computerScore = 0;
+    let playerScore = 0;
+    if (computerSelection === playerSelection) {
+        computerScore = 0;
+        playerScore = 0;
+        console.log("Tie");
+        return [computerScore, playerScore];
+    } else if (computerSelection === "rock" && playerSelection === "scissors"
+        || computerSelection === "paper" && playerSelection === "rock"
+        || computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore = 1;
+        playerScore = 0;
+        console.log("score for computer")
+        return [computerScore, playerScore];
+    } else if (playerSelection === "rock" && computerSelection === "scissors"
+        || playerSelection === "paper" && computerSelection === "rock"
+        || playerSelection === "scissors" && computerSelection === "paper") {
+        computerScore = 0;
+        playerScore = 1;
+        console.log("score for human")
+        return [computerScore, playerScore];
+    } else {
+        // due to lack of constriction to input, other moves will let computer win
+        computerScore = 1;
+        playerScore = 0;
+        console.log("Wrong Move - Computer Wins!")
+        return [computerScore, playerScore];
+    }
+}
+playRound(computerSelection, playerSelection);
 // return string with text that informs about result
 // play 5 rounds, keep score, tell final result, who won the game. 
 
